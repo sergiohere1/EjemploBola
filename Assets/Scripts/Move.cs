6bc3fcafee154827a61d3783682a5e1992c17a1a
sergiohere1 <sergio11garcia@hotmail.com>
2017-12-19 09:49:10 +0100

    1.0

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Los cambios que haya hecho el usuario en el eje horizontal, se los aplico
+        //al de este objeto.
+        transform.Translate(Input.GetAxis("Horizontal") * speed ,0 ,0);
+    }
+}
